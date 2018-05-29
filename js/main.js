fe340af8e141efaaa016eccc24f5a3959eea61cc
wtmjldev <517320676@qq.com>
2018-05-29 16:25:08 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//
+//var bg_height=0;
+//function bg_move(){
+//  var i=$(".c-arrows");
+//  bg_height+=1;
+//	i.css("background","url(img/c-arrows.png) 0 "+bg_height+"px");
+//}
+//setInterval(bg_move,90);
+//console.log(bg_height);
